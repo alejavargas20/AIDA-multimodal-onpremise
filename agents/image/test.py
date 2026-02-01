38edb1278d2fa80e0ac2caacae8e547d66dc2d1d
pauromannebot <paula.roman@nfq.es>
2026-02-01 16:23:32 +0100

    01-02-2026 Nuevas pruebas con distintos ficheros

--- agents/image/test.py
+++ agents/image/test.py
@@ -15,3 +15,11 @@ payload_image = {
 }
 
 print(process(payload_image))
+
+payload_image2 = {
+  "action": "extract_text",
+  "file_path": "desembolsos_ultimo_mes_letra_grande.png",
+  "config": {"use_ocr": True, "language": "es", "max_pages": 5}
+}
+
+print(process(payload_image2))
